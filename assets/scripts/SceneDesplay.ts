0167c22364336d3b1ebffbb624ff7b0847a55e35
杨再涛 <1772081518@qq.com>
2026-09-03 23:19:24 +0800

    初始化

---
+++ assets/scripts/SceneDesplay.ts
@@ -0,0 +1,15 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('SceneDesplay')
+export class SceneDesplay extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
+
